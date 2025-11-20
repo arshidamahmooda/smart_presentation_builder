6569328fc06613c7855f20ca220015b4481ae21f
Arshida Mahmooda <arshidamahmooda548@gmail.com>
2025-11-20 19:45:18 +0530

    Remove font color setting for title slide
    Remove line setting title slide font color to None.

--- models/ppt_generator.py
+++ models/ppt_generator.py
@@ -10,7 +10,6 @@ def generate_presentation_green_theme(slides):
     # Title Slide
     title_slide = prs.slides.add_slide(prs.slide_layouts[0])
     title_slide.shapes.title.text = slides[0]["title"]
-    title_slide.shapes.title.text_frame.paragraphs[0].font.color.rgb = None
 
     # Content Slides
     for slide_data in slides:
